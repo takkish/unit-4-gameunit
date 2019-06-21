2f4c84c20a039b63469dceadba2651b3b1cd657f
takkish <takkish_23@hotmail.com>
2019-06-21 15:32:14 -0700

    commiting

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,65 +1,54 @@
 var win = 0;
 var lose = 0;
 var crystl = 0;
-var scor = 0;
-var targetNumber = Math.floor(Math.random() * 60) + 1;
-var random = Math.floor(Math.random() * 12) + 1;
+var scor;
+var targetNumber;
+
 
 
 $(document).ready(function () {
     function reset() {
+        // when you win or lose
+        // reset score
+        // reset random number
+        // reset random crystal numbers
         scor = 0;
-        targetNumber = Math.floor(Math.random() * 60) + 1;
-        random = Math.floor(Math.random() * 12) + 1;
+        targetNumber =Math.floor(Math.random() * 110) + 20;
+        // define four random numbers for each crystal
+        var random1 = Math.floor(Math.random() * 12) + 1;
+        $("#crystl1").attr("crystl-data",random1);
+        var random2 = Math.floor(Math.random() * 12) + 1;
+        $("#crystl2").attr("crystl-data",random2);
+        var random3 = Math.floor(Math.random() * 12) + 1;
+        $("#crystl3").attr("crystl-data",random3);
+        var random4 = Math.floor(Math.random() * 12) + 1;
+        $("#crystl4").attr("crystl-data",random4);
+        $(".target").text("Target:" + targetNumber);
+        $(".score").text(scor);
     }
-    function next() {
-        targetNumber = Math.floor(Math.random() * 60) + 1;
-        random = Math.floor(Math.random() * 12) + 1;
-    }
+    
+   
     $(".target").text("Target:" + targetNumber)
     // Notice I didn't set $(".jumbotron") to a var this time?
     // If you only plan to use that selector once it doesn't need to be a var
-    $("#crystl1").on("click", function () {
-        scor = scor + random;
-        $(".score").text(scor);
-    });
+    $(".crystal").on("click", function () {
+        var crystldata = $(this).attr("crystl-data");
+        scor = scor + parseInt(crystldata);
+       
 
-    $("#crystl2").on("click", function () {
-        if (".crystal" > 0) {
-            next();
-            scor = scor + random;
-            $(".score").text(scor);
+        $(".score").text(scor);
+        if (scor === targetNumber) {
+            $(".win").text("win:" + ++win)
+            reset();
         }
-        else{
-            scor = scor + random;
-        $(".score").text(scor);
+        else if (scor > targetNumber) {
+            $(".lose").text("lose:" + ++lose)
+            reset();
         }
 
-        
     });
-    $("#crystl3").on("click", function () {
 
-        scor = scor + random;
-        $(".score").text(scor);
-    });
-    $("#crystl4").on("click", function () {
-
-        scor = scor + random;
-        $(".score").text(scor);
-    });
-    if (scor === targetNumber) {
-        alert("you win!")
-        win ++
-        $(".win").text(win);
-    }
-    else{
-        scor>targetNumber
-        alert("you lose")
-        lose ++
-        $(".win").text(lose);
-    }
-    
-
+   
 
 
     reset();
